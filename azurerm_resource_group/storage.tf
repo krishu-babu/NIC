72ca74b9864b316688a0d4f76e6518a3df2f738e
Krishna <chuttanbabu4@gmail.com>
2026-08-10 04:24:14 +0530

    till plan

--- azurerm_resource_group/storage.tf
+++ azurerm_resource_group/storage.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "stg" {
-    depends_on = [azurerm_resource_group.tinku]
-    for_each = var.resource_groups
+  depends_on               = [azurerm_resource_group.tinku]
+  for_each                 = var.resource_groups
   name                     = "fifa"
   resource_group_name      = each.value.name
   location                 = each.value.location
